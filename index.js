1fdfd441197b27203ebb064e04cb826eb77a9331
VladZh97 <zhovklyi.v@gmail.com>
2021-02-19 09:17:50 +0100

    add port

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-let port = 3001
+let port = process.env.PORT || 3001
 const handlebars = require('./handlebars');
 var path = require('path');
 const defaultData = require('./default-data');
